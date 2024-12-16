cc0e068e6c1d2e2e8eab22674a50aa7336f07ea5
cokolatosenak <rw91204@gmail.com>
2024-12-16 08:45:54 +0700

    menambahkan 3 fungsi

---
+++ modells/M_contoh.php
@@ -0,0 +1,19 @@
+<?php
+class m_contoh{
+   public function Tampil()
+   {
+    // fungsinya untuk menampilkan data dari database
+    }
+    public function Edit()
+        {
+            // fungsinya untuk mengedit data dari database
+        }
+    public function Hapus()
+    {
+        // fungsinya untuk menghapus data dari database
+    }
+    
+}
+
+
+?>
